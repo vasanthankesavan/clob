9e7ac8e8a0bec8e5f0b002baf335fbcb655da4fc
Vasanthan Kesavan <vasanthankesavan@gmail.com>
2022-05-20 18:31:44 +0530

    method for returning lowest sell order

--- src/clob.ts
+++ src/clob.ts
@@ -10,11 +10,13 @@ import { AggregatedBook } from "./types/aggregated-book";
 import { timestampFactory } from "./util/timestamp-factory";
 import { Trade } from "./types/trade";
 import { NotYetImplementedError } from "./util/not-yet-implemented-error";
+import { SIDE_BUY } from "./types/side";
 
 /** Single-ticket CLOB order book and trade execution engine */
 export class Clob {
   private readonly logger = new Logger(Clob.name);
-  private _orders: Record<string, Order> = {};
+  private _buyOrders: Record<string, Order> = {};
+  private _sellOrders: Record<string, Order> = {};
 
   public constructor() {
     this.logger.debug("Instantiating");
@@ -46,19 +48,57 @@ export class Clob {
       tradeIds: [],
     };
 
-    this._orders[id] = _order;
+    if (input.side === SIDE_BUY) {
+      this._buyOrders[id] = _order;
+    } else {
+      this._sellOrders[id] = _order; // Makes an assumption that there is only two sides B/S
+    }
+    this.executeOrder(_order);
     return _order;
   }
 
   /** Load an order by its id */
   getOneOrder(orderId: string): Order {
     this.logger.debug(`Loading order with id=${orderId}`);
-    return this._orders[orderId];
+    if (orderId in this._buyOrders) {
+      return this._buyOrders[orderId];
+    } else {
+      return this._sellOrders[orderId];
+    }
   }
 
   /** Load a trade by its id */
   getOneTrade(tradeId: string): Trade {
     this.logger.debug(`Loading trade with id=${tradeId}`);
     throw new NotYetImplementedError();
+  }
+
+  executeOrder(order: Order) {
+    // Buy Order
+    // Execute the order based on lowest price
+    if (order.side === SIDE_BUY) {
+      for(const key in this._sellOrders) {
+
+      }
+    }
+
+    // Sell Order
+    // Execute the order based on highest price
+  }
+
+  getLowestSellOrder(): Order | undefined {
+    let lowestOrderId: string | null = null;
+    let currentLowestPrice: number | null = null;
+
+    for (const key in this._sellOrders) {
+      if (currentLowestPrice) {
+        if(this._sellOrders[key].price < currentLowestPrice) {
+          currentLowestPrice = this._sellOrders[key].price;
+          lowestOrderId = this._sellOrders[key].id;
+        }
+      } 
+    }
+
+    if (lowestOrderId) return this._sellOrders[lowestOrderId];
   }
 }
